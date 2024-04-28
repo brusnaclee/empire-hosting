551da66eac2f00f4dda78ee6de9a016c2d6d67a6
brusnaclee <133024275+brusnaclee@users.noreply.github.com>
2024-04-28 16:18:32 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,68 @@
+module.exports = {
+	TOKEN:
+		'MTA0NDA2MzQxMzgzMzMwMjEwOA.G8xKrr.xri9yZBdVIwAm2TOhZR7AtUlC690EsL2MQfZgg',
+	ownerID: '522711969560657921', //write your discord user id. example: ["id"] or ["id1","id2"]
+	ownerID2: ['522711969560657921', '558280654412840993'],
+	botInvite:
+		'https://discord.com/oauth2/authorize?client_id=1044063413833302108&permissions=414585318465&scope=bot+applications.commands', //write your discord bot invite.
+	supportServer: 'https://empire.is-great.net/', //write your discord bot support server invite.
+	mongodbURL: 'mongodb+srv://brusnaclee:2234@empire.kbvvoal.mongodb.net/', //write your mongodb url.
+	status: 'your music ❤️🎶❤️ ',
+	commandsDir: './commands', //Please don't touch
+	language: 'en', //en, tr, nl, pt, fr, ar, zh_TW, it, ja
+	embedColor: 'FFB238', //hex color code
+	errorLog: '1108313712449831034', // discord error log channel id.
+
+	sponsor: {
+		status: true, //true or false
+		url: 'https://linktr.ee/empiresponsor', //.
+	},
+
+	voteManager: {
+		//optional
+		status: true, //true or false
+		api_key:
+			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEwNDQwNjM0MTM4MzMzMDIxMDgiLCJib3QiOnRydWUsImlhdCI6MTcwODc3NDMxNn0.0Flt_Q8N68A2JSxwt3ia3ZzA5rKyjGaE9ScR-nhq7zI', //write your top.gg api key.
+		vote_commands: [], //write your use by vote commands.
+		vote_url: 'https://top.gg/bot/1044063413833302108/vote', //write your top.gg vote url.
+	},
+
+	shardManager: {
+		shardStatus: false, //If your bot exists on more than 1000 servers, change this part to true.
+	},
+
+	playlistSettings: {
+		maxPlaylist: 50, //max playlist count
+		maxMusic: 500, //max music count
+	},
+
+	opt: {
+		DJ: {
+			commands: [
+				'back',
+				'clear',
+				'filter',
+				'loop',
+				'pause',
+				'resume',
+				'skip',
+				'stop',
+				'volume',
+				'shuffle',
+			], //Please don't touch
+		},
+
+		voiceConfig: {
+			leaveOnFinish: true, //If this variable is "true", the bot will leave the channel the music ends.
+			leaveOnStop: true, //If this variable is "true", the bot will leave the channel when the music is stopped.
+
+			leaveOnEmpty: {
+				//The leaveOnEnd variable must be "false" to use this system.
+				status: true, //If this variable is "true", the bot will leave the channel when the bot is offline.
+				cooldown: 30000, //1000 = 1 second
+			},
+		},
+
+		maxVol: 200, //You can specify the maximum volume level.
+	},
+};
