0faaf54028198b79c3f5c7d4a7996d65b79b0c99
brusnaclee <133024275+brusnaclee@users.noreply.github.com>
2024-05-12 20:59:12 +0700

    Update send-message.js

--- commands/send-message.js
+++ commands/send-message.js
@@ -1,74 +1,109 @@
-const { ApplicationCommandOptionType, DiscordAPIError } = require('discord.js');
+const {
+	ApplicationCommandOptionType,
+	DiscordAPIError,
+	EmbedBuilder,
+} = require('discord.js');
 
 module.exports = {
-  name: 'sm',
-  description: 'Admin command.',
-  options: [
-    {
-      name: 'serverid',
-      description: 'The ID of the server to send the message to.',
-      type: ApplicationCommandOptionType.String,
-      required: true
-    },
-    {
-      name: 'channelid',
-      description: 'The ID of the channel to send the message to.',
-      type: ApplicationCommandOptionType.String,
-      required: true
-    },
-    {
-      name: 'content',
-      description: 'The message content.',
-      type: ApplicationCommandOptionType.String,
-      required: true
-    }
-  ],
-  run: async (client, interaction) => {
-      
-      if (!client.config.ownerID.includes(interaction?.user?.id)) {
-            return interaction
-                .reply({
-                    content: "You don't have permission to use this command.",
-                    ephemeral: true,
-                })
-                .catch((e) => {});
-        } 
-      
-    const serverId = interaction.options.getString('serverid');
-    const channelId = interaction.options.getString('channelid');
-    const messageContent = interaction.options.getString('content');
+	name: 'sm',
+	description: 'Admin command.',
+	options: [
+		{
+			name: 'serverid',
+			description: 'The ID of the server to send the message to.',
+			type: ApplicationCommandOptionType.String,
+			required: true,
+		},
+		{
+			name: 'channelid',
+			description: 'The ID of the channel to send the message to.',
+			type: ApplicationCommandOptionType.String,
+			required: true,
+		},
+		{
+			name: 'title',
+			description: 'The message title.',
+			type: ApplicationCommandOptionType.String,
+			required: true,
+		},
+		{
+			name: 'content',
+			description: 'The message content.',
+			type: ApplicationCommandOptionType.String,
+			required: true,
+		},
+	],
+	run: async (client, interaction) => {
+		if (!client.config.ownerID.includes(interaction?.user?.id)) {
+			return interaction
+				.reply({
+					content: "You don't have permission to use this command.",
+					ephemeral: true,
+				})
+				.catch((e) => {});
+		}
 
-    // Mendapatkan server berdasarkan ID yang diberikan
-    const server = client.guilds.cache.get(serverId);
+		const serverId = interaction.options.getString('serverid');
+		const channelId = interaction.options.getString('channelid');
+		const title = interaction.options.getString('title');
+		const messageContent = interaction.options.getString('content');
 
-    await interaction.deferReply({ ephemeral: true });
+		// Mendapatkan server berdasarkan ID yang diberikan
+		const server = client.guilds.cache.get(serverId);
 
-    // Memeriksa apakah bot ada di server yang ditentukan
-    if (!server) {
-      await interaction.editReply({ content: 'Bot is not in the specified server. <a:Cross:1116983956227772476>', ephemeral: true });
-      return;
-    }
+		await interaction.deferReply({ ephemeral: true });
 
-    // Mendapatkan channel berdasarkan ID yang diberikan
-    const channel = server.channels.cache.get(channelId);
+		// Memeriksa apakah bot ada di server yang ditentukan
+		if (!server) {
+			await interaction.editReply({
+				content:
+					'Bot is not in the specified server. <a:Cross:1116983956227772476>',
+				ephemeral: true,
+			});
+			return;
+		}
 
-    // Memeriksa apakah channel valid
-    if (!channel) {
-      await interaction.editReply({ content: 'Invalid channel ID. <a:Cross:1116983956227772476>', ephemeral: true });
-      return;
-    }
+		// Mendapatkan channel berdasarkan ID yang diberikan
+		const channel = server.channels.cache.get(channelId);
 
-    try {
-      // Mengirim pesan ke channel yang ditentukan
-      await channel.send(messageContent);
-      await interaction.editReply({ content: 'Message has been sent! <a:send:1117660959171956776><a:Ceklis:1116989553744552007>', ephemeral: true });
-    } catch (error) {
-      if (error instanceof DiscordAPIError && error.code === 50013) {
-        await interaction.editReply({ content: 'Bot does not have permission to send messages in the specified channel. <a:Cross:1116983956227772476>', ephemeral: true });
-      } else {
-        console.error('Error sending message:', error);
-        await interaction.editReply({ content: 'An error occurred while sending the message. <a:Cross:1116983956227772476>', ephemeral: true });
-      }
-    }
-  }
+		// Memeriksa apakah channel valid
+		if (!channel) {
+			await interaction.editReply({
+				content: 'Invalid channel ID. <a:Cross:1116983956227772476>',
+				ephemeral: true,
+			});
+			return;
+		}
+
+		const embed = new EmbedBuilder()
+			.setColor('FFB238')
+			.setTitle(title)
+			.setDescription(messageContent)
+			.setFooter({ text: 'Empire ❤️' });
+
+		try {
+			// Mengirim pesan ke channel yang ditentukan
+			await channel.send({ embeds: [embed] });
+			await interaction.editReply({
+				content:
+					'Message has been sent! <a:send:1117660959171956776><a:Ceklis:1116989553744552007>',
+				ephemeral: true,
+			});
+		} catch (error) {
+			if (error instanceof DiscordAPIError && error.code === 50013) {
+				await interaction.editReply({
+					content:
+						'Bot does not have permission to send messages in the specified channel. <a:Cross:1116983956227772476>',
+					ephemeral: true,
+				});
+			} else {
+				console.error('Error sending message:', error);
+				await interaction.editReply({
+					content:
+						'An error occurred while sending the message. <a:Cross:1116983956227772476>',
+					ephemeral: true,
+				});
+			}
+		}
+	},
 };
