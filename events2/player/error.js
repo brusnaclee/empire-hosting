798acb3c769caa39b7f264fb3f6c5a606f80d106
brusnaclee <133024275+brusnaclee@users.noreply.github.com>
2024-07-15 15:15:42 +0700

    Delete events2 directory

--- events2/player/error.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (client, textChannel, e) => {
-	if (textChannel) {
-		console.log(`${e.toString().slice(0, 1974)}`);
-		console.log(e);
-		return textChannel?.send(
-			`**An error encountered:** ${e.toString().slice(0, 1974)}`
-		);
-	}
-};
