499b76f931829d187319fdbca63ca72330de346a
brusnaclee <133024275+brusnaclee@users.noreply.github.com>
2024-09-20 11:36:18 +0700

    Update cv.js

--- commands/cv.js
+++ commands/cv.js
@@ -2,7 +2,7 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	name: 'cv',
-	description: 'Admin command.',
+	description: 'Developer command.',
 	permissions: '0x0000000000000800',
 	run: async (client, interaction) => {
 		if (!client.config.ownerID.includes(interaction?.user?.id)) {
@@ -16,35 +16,27 @@ module.exports = {
 
 		const botChannelInfo = [];
 
-		// Memeriksa apakah bot sedang terhubung ke saluran suara pada setiap server
 		client.guilds.cache.forEach((guild) => {
 			const botVoiceChannel = guild.voiceStates.cache.get(client.user.id);
 
-			if (botVoiceChannel && botVoiceChannel.channel) {
-				// Mengambil nama saluran suara dan nama server
-				const channelName = botVoiceChannel.channel.name;
-				const guildName = guild.name;
-
-				// Menambahkan informasi saluran suara dan server ke dalam array
-				botChannelInfo.push(`${channelName} - ${guildName}`);
+			if (botVoiceChannel) {
+				botChannelInfo.push(`${guild.name}: ${botVoiceChannel.channel.name}`);
 			}
 		});
 
-		// Membuat pesan embed dengan informasi saluran suara yang bot masuki
 		const embed = new EmbedBuilder()
 			.setTitle("Bot's Voice Channel Info")
 			.setColor(client.config.embedColor)
 			.setDescription(
-				botChannelInfo.length
-					? botChannelInfo.join('\n')
-					: 'Bot is not in a voice channel in any server.'
+				botChannelInfo.join('\n') ||
+					'Bot is not in a voice channel in any server.'
 			)
 			.setTimestamp()
 			.setFooter({ text: `Empire ❤️` });
 
 		try {
-			await interaction.deferReply({ ephemeral: false });
-			await interaction.editReply({ embeds: [embed], ephemeral: false });
+			await interaction.deferReply({ ephemeral: false }); // Menunda respon dan mengatur ephemeral menjadi false
+			await interaction.editReply({ embeds: [embed], ephemeral: false }); // Mengedit respons dengan embed dan ephemeral false
 		} catch (error) {
 			const errorNotifier = require('../functions.js');
 			errorNotifier(client, interaction, error, lang);
