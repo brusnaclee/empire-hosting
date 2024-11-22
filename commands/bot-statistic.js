247bc40b04dc588a1fa8c783d9bae588a1545395
brusnaclee <133024275+brusnaclee@users.noreply.github.com>
2024-11-23 01:39:20 +0700

    Update bot-statistic.js

--- commands/bot-statistic.js
+++ commands/bot-statistic.js
@@ -1,79 +1,42 @@
-const config = require('../config.js');
-const db = require('../mongoDB');
-const os = require('os');
 const { exec } = require('child_process');
+const os = require('os');
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const config = require('../config.js');
 
 module.exports = {
-	name: 'statistic',
-	description: 'View the bot statistics.',
-	options: [],
-	run: async (client, interaction) => {
-		let lang = await db?.musicbot
-			?.findOne({ guildID: interaction.guild.id })
-			.catch(() => {});
-		lang = lang?.language || client.language;
-		lang = require(`../languages/${lang}.js`);
+    name: 'statistic',
+    description: 'View the bot statistics.',
+    options: [],
+    permissions: '0x0000000000000800',
+    run: async (client, interaction) => {
+        let lang = await db?.musicbot?.findOne({ guildID: interaction.guild.id }).catch((e) => {});
+        lang = lang?.language || client.language;
+        lang = require(`../languages/${lang}.js`);
 
-		try {
-			const {
-				ActionRowBuilder,
-				ButtonBuilder,
-				EmbedBuilder,
-				ButtonStyle,
-			} = require('discord.js');
+        try {
+            // Fetching system stats (CPU, Memory, etc)
+            exec('lscpu | grep -v "Flags"', (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                    return;
+                }
+                const cpuInfo = stdout; // Collecting the filtered CPU info
 
-			// Initialize bot stats
-			let totalGuilds = client.guilds.cache.size;
-			let totalMembers = client.guilds.cache.reduce(
-				(acc, guild) => acc + guild.memberCount,
-				0
-			);
-			let totalChannels = client.guilds.cache.reduce(
-				(acc, guild) => acc + guild.channels.cache.size,
-				0
-			);
-			let shardSize = 1;
-			let voiceConnections = client?.voice?.adapters?.size || 0;
+                // Fetching memory info
+                exec('free -m', (error, stdout, stderr) => {
+                    if (error) {
+                        console.error(`exec error: ${error}`);
+                        return;
+                    }
+                    const memoryInfo = stdout; // Collecting the memory info
 
-			// Fetch memory usage
-			const usedMemory = ((os.totalmem() - os.freemem()) / 1024 / 1024).toFixed(
-				2
-			);
-
-			// Defer the reply to acknowledge the interaction
-			await interaction.deferReply();
-
-			// Execute Linux commands for detailed OS and CPU info
-			exec('lsb_release -a', (error, stdout) => {
-				if (error) {
-					console.error(error);
-					return;
-				}
-				const osInfoLines = stdout.split('\n');
-				const descriptionLine = osInfoLines.find((line) =>
-					line.startsWith('Description:')
-				);
-				const osVersion = descriptionLine
-					? descriptionLine.split(':')[1].trim()
-					: 'Unknown';
-
-				exec('lscpu', (error, cpuInfo) => {
-					if (error) {
-						console.error(error);
-						return;
-					}
-
-					// General Embed
-					const embed = new EmbedBuilder()
-						.setTitle(
-							`<a:Statisticreverse:1117043433022947438> ${client.user.username} Statistics <a:Statistic:1117010987040645220>`
-						)
-						.setThumbnail(
-							client.user.displayAvatarURL({ dynamic: true, size: 1024 })
-						)
-						.setColor(client.config.embedColor)
-						.setDescription(
-							`**General Information:\n\n
+                    // Embed message
+                    const embed = new EmbedBuilder()
+                        .setTitle(`<a:Statisticreverse:1117043433022947438> ${client.user.username} ${lang.msg19} <a:Statistic:1117010987040645220>`)
+                        .setThumbnail(client.user.displayAvatarURL({ dynamic: true, size: 1024 }))
+                        .setColor(client.config.embedColor)
+                        .setTimestamp()
+                        .setDescription(`**General Information:\n\n
 • Owner: \`brusnaclee#0\`
 • Developer: \`brusnaclee#0\`
 • User Count: \`${totalMembers || 0}\`
@@ -85,48 +48,47 @@ module.exports = {
 • Operation Time: <t:${Math.floor(Number(Date.now() - client.uptime) / 1000)}:R>
 • Ping: \`${client.ws.ping} MS\`
 • Invite Bot: [Click](${config.botInvite})
-• Website Bot: [Click](${config.supportServer})
+• Website bot: [Click](${config.supportServer})
 ${
-	config.sponsor.status ? `• Sponsor: [Click](${config.sponsor.url})` : ''
+    config.sponsor.status == true
+    ? `• Sponsor: [Click](${config.sponsor.url})`
+    : ``
 }
 ${
-	config.voteManager.status
-		? `• Vote: [Click](${config.voteManager.vote_url})`
-		: ''
+    config.voteManager.status == true
+    ? `• Vote: [Click](${config.voteManager.vote_url})`
+    : ``
 }
-**`
-						);
+**`); // Add general stats
 
-					// Buttons
-					const generalButton = new ButtonBuilder()
-						.setCustomId('general_info')
-						.setLabel('General Information')
-						.setStyle(ButtonStyle.Success);
+                    // Buttons for interaction
+                    const generalButton = new ButtonBuilder()
+                        .setCustomId('general_info')
+                        .setLabel('General Information')
+                        .setStyle(ButtonStyle.Success);
 
-					const hardwareButton = new ButtonBuilder()
-						.setCustomId('hardware_info')
-						.setLabel('Hardware Information')
-						.setStyle(ButtonStyle.Success);
+                    const hardwareButton = new ButtonBuilder()
+                        .setCustomId('hardware_info')
+                        .setLabel('Hardware Information')
+                        .setStyle(ButtonStyle.Success);
 
-					const buttonRow = new ActionRowBuilder().addComponents(
-						generalButton,
-						hardwareButton
-					);
+                    const buttonRow = new ActionRowBuilder().addComponents(generalButton, hardwareButton);
 
-					// Send initial embed with buttons
-					interaction.editReply({ embeds: [embed], components: [buttonRow] });
+                    interaction.deferReply({ content: 'loading' });
 
-					// Collector for button interactions
-					const collector = interaction.channel.createMessageComponentCollector({
-						filter: (i) => i.user.id === interaction.user.id,
-						time: 120000, // 2 minutes
-					});
+                    interaction.editReply({
+                        embeds: [embed],
+                        components: [buttonRow],
+                    }).then(() => {
+                        const filter = (interaction) => interaction.user.id === interaction.user.id;
+                        const collector = interaction.channel.createMessageComponentCollector({
+                            filter,
+                            time: 120000, // 2 minutes
+                        });
 
-					collector.on('collect', async (i) => {
-						if (i.customId === 'general_info') {
-							// Update embed to General Information
-							embed.setDescription(
-								`**General Information:\n\n
+                        collector.on('collect', async (interaction) => {
+                            if (interaction.customId === 'general_info') {
+                                embed.setDescription(`**General Information:\n\n
 • Owner: \`brusnaclee#0\`
 • Developer: \`brusnaclee#0\`
 • User Count: \`${totalMembers || 0}\`
@@ -138,47 +100,47 @@ ${
 • Operation Time: <t:${Math.floor(Number(Date.now() - client.uptime) / 1000)}:R>
 • Ping: \`${client.ws.ping} MS\`
 • Invite Bot: [Click](${config.botInvite})
-• Website Bot: [Click](${config.supportServer})
+• Website bot: [Click](${config.supportServer})
 ${
-	config.sponsor.status ? `• Sponsor: [Click](${config.sponsor.url})` : ''
+    config.sponsor.status == true
+    ? `• Sponsor: [Click](${config.sponsor.url})`
+    : ``
 }
 ${
-	config.voteManager.status
-		? `• Vote: [Click](${config.voteManager.vote_url})`
-		: ''
+    config.voteManager.status == true
+    ? `• Vote: [Click](${config.voteManager.vote_url})`
+    : ``
 }
-**`
-							);
-							await i.update({ embeds: [embed] });
-						} else if (i.customId === 'hardware_info') {
-							// Update embed to Hardware Information
-							embed.setDescription(
-								`**Hardware Information:\n\n
-• CPU Info: \`${cpuInfo.trim()}\`
-• Host Memory Usage: \`${usedMemory} MB\`
-• Bot Memory Usage: \`${(
-									process.memoryUsage().rss /
-									1024 /
-									1024
-								).toFixed(2)} MB\`
+**`);
+                                await interaction.update({ embeds: [embed] });
+                            } else if (interaction.customId === 'hardware_info') {
+                                // Updating the hardware info section
+                                embed.setDescription(`**Hardware Information:\n\n
+• CPU Info: \`\`\`${cpuInfo}\`\`\`
+• Host Memory Usage: \`${memoryInfo}\`
 • Architecture: \`${os.arch()}\`
-• OS Version: \`${osVersion}\`
-**`
-							);
-							await i.update({ embeds: [embed] });
-						}
-					});
+• OS Version: \`${os.version()}\`
+**`);
+                                await interaction.update({ embeds: [embed] });
+                            }
+                        });
 
-					collector.on('end', () => {
-						interaction.editReply({ components: [] }).catch(console.error);
-					});
-				});
-			});
-		} catch (e) {
-			console.error(e);
-			await interaction.editReply({
-				content: 'An error occurred while fetching statistics.',
-			});
-		}
-	},
+                        collector.on('end', async () => {
+                            await interaction.editReply({
+                                components: [],
+                            }).then(() => {
+                                setTimeout(async () => {
+                                    await interaction.deleteReply().catch((err) => console.error(err));
+                                }, 5000);
+                            }).catch((err) => console.error(err));
+                        });
+                    });
+                });
+            });
+
+        } catch (e) {
+            const errorNotifer = require('../functions.js');
+            errorNotifer(client, interaction, e, lang);
+        }
+    },
 };
